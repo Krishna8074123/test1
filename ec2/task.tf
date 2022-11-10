8b5b82f361f7de380b34e536d7831d4b2a557d1f
krishna <gangulak36@gmail.com>
2022-11-10 21:56:47 +0530

    add

--- ec2/task.tf
+++ ec2/task.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_vpc" "aws_vpc" {
   cidr_block = "10.10.0.0/16"
-  enable_dns_hostnames = true
+  # enable_dns_hostnames = true
   tags = {
     Name = "aws_vpc"
   }
