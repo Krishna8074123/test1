e6e55643dbeb62f23ffb5671fdd7e1bade81ac18
krishna <gangulak36@gmail.com>
2022-12-09 21:43:38 +0530

    commit dooo

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,13 +1,13 @@
 pipeline{
     agent {label 'docker'}
     stages{
-        stage('branch'){
+        stage("branch"){
             steps{
                 git url: "https://github.com/Krishna8074123/test1.git",
                 branch: "master"
             }
         }
-        stage('docker'){
+        stage("docker"){
             steps{
                 sh script: "docker info"
             }
