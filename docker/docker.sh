9c27f1ac65965a4576d5a290e065d8b5c258d5b8
krishna <gangulak36@gmail.com>
2022-12-09 21:41:55 +0530

    mnsndnsfndn

--- docker/docker.sh
+++ docker/docker.sh
@@ -3,8 +3,8 @@ pipeline{
     stages{
         stage('branch'){
             steps{
-                git url:"https://github.com/Krishna8074123/test1.git",
-                branch:"master"
+                git url: "https://github.com/Krishna8074123/test1.git",
+                branch: "master"
             }
         }
         stage('docker'){
