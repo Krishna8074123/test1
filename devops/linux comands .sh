6a0ed6de6002522def1ceebd37b4cfd00f0fdee1
krishna <gangulak36@gmail.com>
2023-03-16 17:03:57 +0530

    this is linux commands

--- devops/linux comands .sh
+++ devops/linux comands .sh
@@ -1,39 +1,40 @@
-linux commands
-ls: list files and directories in the current directory
-ls -al :to show the all current directoriesire
-ls -lrt
-cd: change directory
-pwd: print the current working directory
-mkdir: make a new directory
-rm: remove a file or directory
-cp: copy a file or directory
-mv: move a file or directory
-touch: create a new empty file or update the modification time of an existing file
-cat: print the contents of a file to the terminal
-grep: search for a pattern in a file
-echo: print a message to the terminal
-chmod: change the permissions of a file or directory
-chown: change the owner of a file or directory
-sudo: run a command with administrative privileges
-ssh: securely connect to a remote server or device
-  apt
-  yum
-scp: Copy files between local and remote systems securely.
-top: display real-time system resource usage
-ps: list running processes
-kill: terminate a running process
-ping: test network connectivity to a server
-traceroute: display the route that network packets take to reach a destination.
-mount: Mount a file system.
-umount: Unmount a file system.
-mount:used to mount file systems,including network file systems, onto the directory tree. When a file system is mounted, 
-its files and directories become accessible as if they were part of the directory tree of the mounted file system.
-   "-t" or "--type": Specify the file system type (e.g. ext4, nfs, cifs, etc.) to be mounted.
-   "-o" or "--options": Specify mount options (e.g. read-only, noexec, remount, etc.) for the file system to be mounted.
-df: used to display information about the available disk space on the file system(s) that are currently mounted
-  "-h" or "--human-readable": Display the disk space in a human-readable format (e.g. "10G" instead of "10737418240").
-  "-T" or "--print-type": Display the file system type along with the disk space information.
-  "-i" or "--inodes": Display information about the number of free and used inodes 
-(i.e. data structures used by the file system to store information about files and directories).
-
-
+#  linux commands
+*  ls: list files and directories in the current directory
+*  ls -al :to show the all current directoriesire
+*  ls -lrt
+*  cd: change directory
+*  pwd: print the current working directory
+*  mkdir: make a new directory
+*  rm: remove a file or directory
+*  cp: copy a file or directory
+*  mv: move a file or directory
+*  touch: create a new empty file or update the modification time of an existing file
+*  cat: print the contents of a file to the terminal
+*  grep: search for a pattern in a file
+*  echo: print a message to the terminal
+*  chmod: change the permissions of a file or directory
+*  chown: change the owner of a file or directory
+*  sudo: run a command with administrative privileges
+*  ssh: securely connect to a remote server or device
+*    apt
+*    yum
+*  scp: Copy files between local and remote systems securely.
+*  top: display real-time system resource usage
+*  ps: list running processes
+*  kill: terminate a running process
+*  ping: test network connectivity to a server
+*  traceroute: display the route that network packets take to reach a destination.
+*  mount: Mount a file system.
+*  umount: Unmount a file system.
+*  mount:used to mount file systems,including network file systems, onto the directory tree. When a file system is mounted, 
+*  its files and directories become accessible as if they were part of the directory tree of the mounted file system.
+*    "-t" or "--type": Specify the file system type (e.g. ext4, nfs, cifs, etc.) to be mounted.
+*    "-o" or "--options": Specify mount options (e.g. read-only, noexec, remount, etc.) for the file system to be mounted.
+*  df: used to display information about the available disk space on the file system(s) that are currently mounted
+*    "-h" or "--human-readable": Display the disk space in a human-readable format (e.g. "10G" instead of "10737418240").
+*    "-T" or "--print-type": Display the file system type along with the disk space information.
+*    "-i" or "--inodes": Display information about the number of free and used inodes 
+*  (i.e. data structures used by the file system to store information about files and directories).
+*
+*
+*
